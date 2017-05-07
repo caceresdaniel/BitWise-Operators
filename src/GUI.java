7d5bfb43f7f71340626520c8b1cb0dd230398dc9
caceresdaniel <mattmoocow@live.com>
2017-05-06 22:46:37 -0700

    buttons

--- src/GUI.java
+++ src/GUI.java
@@ -1,49 +1,96 @@
 import java.io.File;
-import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class GUI extends Application{
-	
-	File file = new File("sepiacat.ppm");
-	SubPPMImage sub = new SubPPMImage(file);
+public class GUI extends Application {
+
+	HBox buttons = new HBox();
+	HBox imageContainer = new HBox();
+
+	Pane pane = new Pane();
+
+	File file;
+
+	ImageView imgview = new ImageView();
+
+	SubPPMImage sub;
+
+	Button sepButt = new Button("Sepia");
+	Button negButt = new Button("Negative");
+	Button grayButt = new Button("Gray Scale");
+	Button enButt = new Button("Hide Message");
+	Button grabButt = new Button("Select File");
+	Button readButt = new Button("Read Message");
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		primaryStage.setTitle("BitWise Encoding?");
+
+		Scene sc = new Scene(pane, 1000, 1000);
+
+		buttons.getChildren().addAll(grabButt, enButt, readButt, grayButt, negButt, sepButt);
 		
-		Pane pane = new Pane();
-		Scene sc = new Scene(pane);
-		Image image = SwingFXUtils.toFXImage(ImageIO.read(file), null);
-		
-		ImageView imgview = new ImageView();
-		
-		imgview.setImage(image);
-		
-		pane.getChildren().add(imgview);
-		
-		//main();
-		
+		pane.getChildren().addAll(buttons, imageContainer);
+
+		grabButton();
+		sepButton();
+		grayButton();
+		negButton();
+
 		primaryStage.setScene(sc);
 		primaryStage.show();
 	}
-	
-	public void main(){
 
-		//sub.negative();
-		//sub.grayscale();
-		sub.sepia();
+	private void grabButton() {
+		grabButt.setOnMouseClicked(e -> {
+			JFileChooser fc = new JFileChooser();
+			int val = fc.showOpenDialog(null);
+
+			if (val == JFileChooser.APPROVE_OPTION) {
+				file = fc.getSelectedFile();
+				sub = new SubPPMImage(file);
+			}
+
+			try {
+				Image image = SwingFXUtils.toFXImage(ImageIO.read(file), null);
+
+				imgview.setImage(image);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		});
 	}
-	
+
+	private void sepButton() {
+		sepButt.setOnMouseClicked(e -> {
+			sub.sepia();
+		});
+	}
+
+	private void grayButton() {
+		grayButt.setOnMouseClicked(e -> {
+			sub.grayscale();
+		});
+	}
+
+	private void negButton() {
+		negButt.setOnMouseClicked(e -> {
+			sub.negative();
+		});
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
